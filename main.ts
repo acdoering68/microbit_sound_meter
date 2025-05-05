b1c793ce57288f6b5337b7d4b428b0eabbc01892
Andreas Doering <130436425+acdoering68@users.noreply.github.com>
2025-05-05 10:26:30 +0200

    Serial output of period added

--- main.ts
+++ main.ts
@@ -1,29 +1,35 @@
-let soundsamples = [];
-let rt0 = 0;
-let firstmax = 0;
-let secondmax = 0;
-let period = 1;
-let i = 0;
-while (true) {
-    rt0 = input.runningTime();
+let ss = 0
+let rt0 = 0
+let firstmax = 0
+let secondmax = 0
+let i = 0
+let soundsamples = [0]
+let period = 1
+basic.forever(function () {
+    rt0 = input.runningTime()
+    soundsamples = [];
     while (input.runningTime() < rt0 + 1000) {
-        soundsamples.push(input.soundLevel())
+        ss = input.soundLevel()
+        soundsamples.push(ss)
         pause(1);
     }
-    firstmax = 0; secondmax = 1
+    firstmax = 0
+    secondmax = 1
     for (i = 0; i < soundsamples.length - 1; i++) {
-        if ((soundsamples[i] > soundsamples[i - 1]) &&
-            (soundsamples[i] > soundsamples[i + 1])) {
-            firstmax = i;
+            if ((soundsamples[i] > soundsamples[i - 1]) &&
+                (soundsamples[i] > soundsamples[i + 1])) {
+                firstmax = i;
+            }
         }
-    }
-    for (i = firstmax + 1; i < soundsamples.length - 1; i++)
-        if ((soundsamples[i] > soundsamples[i - 1]) &&
-            (soundsamples[i] > soundsamples[i + 1])) {
-            secondmax = i;
-        }
-    serial.writeLine(firstmax.toString()  + " " + secondmax.toString());
-    period = secondmax - firstmax;
-    basic.clearScreen();
-    led.plot((period / 20) % 4, period / 80);
-}
+        secondmax = firstmax;
+for (i = firstmax + 1; i < soundsamples.length - 1; i++)
+            if ((soundsamples[i] > soundsamples[i - 1]) &&
+                (soundsamples[i] > soundsamples[i + 1])) {
+                secondmax = i;
+            }
+period = secondmax - firstmax
+    serial.writeValue("Period",period);
+    serial.writeValue("firstSoundSample",soundsamples[0]);
+    basic.clearScreen()
+    led.plot(period / 20 % 4, period / 80)
+})
