30a67947cdccb47ceb64db8eee8c39b5b64760b0
Andreas Doering <130436425+acdoering68@users.noreply.github.com>
2025-05-05 10:39:54 +0200

    Scale of period to LED adapted

--- main.ts
+++ main.ts
@@ -6,30 +6,33 @@ let i = 0
 let soundsamples = [0]
 let period = 1
 basic.forever(function () {
-    rt0 = input.runningTime()
+    
     soundsamples = [];
-    while (input.runningTime() < rt0 + 1000) {
+    for (i=0; i < 1000; i++)  {
         ss = input.soundLevel()
         soundsamples.push(ss)
         pause(1);
     }
     firstmax = 0
     secondmax = 1
-    for (i = 0; i < soundsamples.length - 1; i++) {
-            if ((soundsamples[i] > soundsamples[i - 1]) &&
+    for (i = 1; i < soundsamples.length - 1; i++) {
+            if ((soundsamples[i] >= soundsamples[i - 1]) &&
                 (soundsamples[i] > soundsamples[i + 1])) {
                 firstmax = i;
+                break;
             }
         }
         secondmax = firstmax;
 for (i = firstmax + 1; i < soundsamples.length - 1; i++)
-            if ((soundsamples[i] > soundsamples[i - 1]) &&
+            if ((soundsamples[i] >= soundsamples[i - 1]) &&
                 (soundsamples[i] > soundsamples[i + 1])) {
                 secondmax = i;
+                break;
             }
 period = secondmax - firstmax
+    serial.writeValue("FirstMax",firstmax);
     serial.writeValue("Period",period);
     serial.writeValue("firstSoundSample",soundsamples[0]);
     basic.clearScreen()
-    led.plot(period / 20 % 4, period / 80)
+    led.plot(period % 4, period /4)
 })
